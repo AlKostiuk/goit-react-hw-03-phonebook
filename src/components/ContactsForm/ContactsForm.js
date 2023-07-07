cd74375702bae62f385faede16194170b6f69350
AlKostiuk <Kostiuk.A96@gmail.com>
2023-07-07 16:05:36 +0300

    improve pattern

--- src/components/ContactsForm/ContactsForm.js
+++ src/components/ContactsForm/ContactsForm.js
@@ -35,7 +35,7 @@ class ContactsForm extends Component{
   type="text"
   id="Username"
   name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  pattern= "^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
   required
     />
@@ -45,7 +45,8 @@ class ContactsForm extends Component{
     id="Username"
   type="tel"
   name="number"
-  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}" 
+
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
   required
 />
